635eb9c230272097969e50b985e39d78d9c727a9
Aadarsh <aadarsh9811594152@gmail.com>
2022-11-13 03:51:07 +0530

    task-08 completed

---
+++ task-08/index.js
@@ -0,0 +1,37 @@
+
+
+function playMyAudio1(){
+  document.getElementById("myAudio1").play();
+}
+
+function playMyAudio2(){
+    document.getElementById("myAudio2").play();
+  }
+  
+
+  function playMyAudio3(){
+    document.getElementById("myAudio3").play();
+  }
+  
+
+  function playMyAudio4(){
+    document.getElementById("myAudio4").play();
+  }
+  
+
+  function playMyAudio5(){
+    document.getElementById("myAudio5").play();
+  }
+  
+
+  function playMyAudio6(){
+    document.getElementById("myAudio6").play();
+  }
+  
+
+  function playMyAudio7(){
+    document.getElementById("myAudio7").play();
+  }
+  
+            
+
