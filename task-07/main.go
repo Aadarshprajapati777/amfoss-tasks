023474edebd2528305574247bb61eff543dd7e1f
Aadarsh <aadarsh9811594152@gmail.com>
2022-12-03 05:48:59 +0530

    function is working

--- task-07/main.go
+++ task-07/main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
-	"net/http"
+	"syscall/js"
 )
 
-func incrementfunc() {
-	fmt.println("increment is clicked")
+func add(this js.Value, args []js.Value) interface{} {
+	println("add called")
+	return args[0].Int() + args[1].Int()
 }
-
+func registerCallbacks() {
+	js.Global().Set("add", js.FuncOf(add))
+}
 func main() {
-	http.ListenAndServe(":8080", http.FileServer(http.Dir(".")))
-	incrementfunc()
+	c := make(chan struct{}, 0)
+	registerCallbacks()
+	<-c
 }
