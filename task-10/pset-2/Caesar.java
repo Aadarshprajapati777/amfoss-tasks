a7c599e09689ab6bcddae274b6d3674474a57b87
Aadarsh <aadarsh9811594152@gmail.com>
2022-12-08 23:29:26 +0530

    cs50 week problem solved

---
+++ task-10/pset-2/Caesar.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Caesar{
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Enter a string: ");
+    String str = scanner.nextLine();
+
+    System.out.print("Enter an int: ");
+    int num = scanner.nextInt();
+
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < str.length(); i++) {
+      char ch = str.charAt(i);
+      ch = (char)(ch + num);
+      sb.append(ch);
+    }
+    System.out.println(sb);
+  }
+}
